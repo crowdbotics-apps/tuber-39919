c39cc0521ba2df305f725dab0aa9463fbe6f7aa0
Anand Kulkarni <anand@crowdbotics.com>
2023-04-13 20:04:30 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "tuber-39919",
-  url: "https://tuber-39919.botics.co",
-  api: "https://tuber-39919.botics.co/api/v1"
-}
+  "name": "tuber-39919",
+  "url": "https://tuber-39919.botics.co",
+  "api": "https://tuber-39919.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
