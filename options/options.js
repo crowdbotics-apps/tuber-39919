fecfc79c0a4921682be9a55dd037fc4531922fa6
Crowdbotics <team@crowdbotics.com>
2023-04-14 03:01:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tuber-39919",
+  url: "https://tuber-39919.botics.co",
+  api: "https://tuber-39919.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
